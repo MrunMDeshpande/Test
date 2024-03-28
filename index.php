019825dc811262a0bfd31e1f6fa07adc04041d68
MrunMDeshpande <165160511+MrunMDeshpande@users.noreply.github.com>
2024-03-28 14:58:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <html>
 <head>
-<title>Welcome to Vinod's test web site </title>
+<title>Welcome to Mruns's test web site </title>
 </head>
 <body>
  
-<h1>Welcome to Vinod's test web site</h1>
-<p>Welcome to Vinod's test web site.</p>
+<h1>Welcome to Mruns's test web site</h1>
+<p>Welcome to Mrun's test web site.</p>
  
 </body>
 </html>
